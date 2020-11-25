072efbc570bad391b0677e6d7739d03a1d35d76b
조선근 <eddy0215@kakao.com>
2020-11-25 14:08:21 +0900

    [서울]AVR_LED_인터럽트추가

--- 서울/AVR/2020/led.c
+++ 서울/AVR/2020/led.c
@@ -3,31 +3,36 @@
 #include <io.h>
 #include <delay.h>
 #include <stdio.h>
+#include <interrupt.h>
 
 #define DELAY 150
+int sel = 1;
 
+interrupt [EXT_INT4] void ext_int4_isr(void)
+{
+    sel = 1;
+}
+
+interrupt [EXT_INT5] void ext_int5_isr(void)
+{
+    sel = 2;
+}
+
+void init(){
+    DDRA = 0xff;
+    DDRE = 0x00;  
+    EICRB = (1<<ISC41)|(1<<ISC51); 
+    EIMSK = (1<<INT4)|(1<<INT5);  
+    sei();
+}
+    
 void main(void)
 {
-    int i=0;
-    int sel=0,led=0x01,dir=0;
-    DDRA = 0xff;
-    DDRE = 0x00;
+    int led=0x01,dir=0;
+    
+    init();    
     while (1)
-    {  	
-        if ((PINE & 0x10) == 0x00){ 	// SW1 = PE bit4
-            PORTA = 0xff;		// LED = ¡®ON¡¯
-            sel = 1;
-        }
-        else
-            PORTA = 0x00;		// LED = ¡®OFF¡¯
-                  
-        if ((PINE & 0x20) == 0x00){ 	// SW1 = PE bit4
-            PORTA = 0xff;		// LED = ¡®ON¡¯
-            sel = 2;
-        }
-        else
-            PORTA = 0x00;		// LED = ¡®OFF¡¯
-                   
+    {  	        
         if(sel == 1){
             if(dir == 0){
                 PORTA = led; 
@@ -46,21 +51,6 @@ void main(void)
                     led = 0x01;
                 }
             }            
-            
-        /*
-            if(dir == 0){    
-                PORTA = (led << 1);
-                delay_ms(200);
-                if(PORTA == 0x80)
-                    dir = 1;
-            }
-            if(dir == 1){
-                PORTA = (led >> 1);
-                delay_ms(200);
-                if(PORTA == 0x01)
-                    dir = 0;
-            }
-        */
         }        
          if(sel == 2){
             PORTA = 0xAA;            
